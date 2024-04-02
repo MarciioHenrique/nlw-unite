7b571dfd770765b1217a15a0b5b1958501fc06a6
Marcio Henrique <dev.marcioh@gmail.com>
2024-04-02 19:12:22 -0300

    fix: slug sendo gerado dinâmicamente

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify()
 
@@ -17,12 +18,24 @@ app.post("/events", async (request, reply) => {
 
     const data = createEventSchema.parse(request.body)
 
+    const slug = generateSlug(data.title)
+
+    const eventWithSameSlug = await prisma.event.findUnique({
+        where: {
+            slug
+        }
+    })
+
+    if (eventWithSameSlug !== null) {
+        throw new Error("Another event has the same title")
+    }
+
     const event = await prisma.event.create({
         data: {
             title: data.title,
             details: data.details,
             maximumAttendees: data.maximumAttendees,
-            slug: new Date().toISOString()
+            slug
         }
     })
 
